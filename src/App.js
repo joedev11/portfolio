1467002baac20afd4940bcf13eb8ac9f509f1ded
joel-dimayuga <john.dimayuga@awsys-i.com>
2023-11-07 22:51:39 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar'
+import Header from './Components/Header'
+import AboutMe from './Components/AboutMe'
+import Education from './Components/Education'
+import Footer from'./Components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App  bg-black">
+      <main className="flex min-h-screen min-w-screen flex-col items-center p-6 max-w-[1200px] mx-auto relative">
+        <Navbar />
+        <Header />
+        <AboutMe />
+        <Education />
+        <Footer />
+      </main>
     </div>
   );
 }
